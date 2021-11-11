07faa6c6bc59594efb254718a06e42829cc395b9
Steve Haroz <painbow@steveharoz.com>
2021-11-11 18:40:02 +0000

    version 1.0.1

---
+++ R/painbow_data.R
@@ -0,0 +1,19 @@
+#' A 2D heatmap from XKCD's painbow comic
+#'
+#' @format A data frame with 58,425 rows and 3 variables:
+#' \describe{
+#'   \item{x}{x-axis position}
+#'   \item{y}{y-axis position}
+#'   \item{value}{A value from 0 to 1}
+#' }
+#' @source \url{https://xkcd.com/2537/}
+"painbow_data"
+
+
+#' An array of colors from XKCD's painbow comic
+#'
+#' @format An array with 192 hex color strings.
+#' @description This set of 192 colors is the Painbow. The colors were extracted using the pixels from the color bar on the right side of the comic.
+#' @source \url{https://xkcd.com/2537/}
+"painbow_colors"
+
